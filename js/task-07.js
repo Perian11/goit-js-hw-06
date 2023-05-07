0284c5fab7ad7675f05801ab96843de31c4f294b
Perian11 <121199037+Perian11@users.noreply.github.com>
2023-05-07 13:34:39 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,9 @@
 const fontSizeCtrl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
- fontSizeCtrl .addEventListener("input", () => {
-    const fontSize = fontSizeCtrl.value + "px";
-    text.style.fontSize = fontSize;
- });
+const initialFontSize = fontSizeCtrl.value + "px";
+text.style.fontSize = initialFontSize;
 
-
-
-
-
-
+fontSizeCtrl.addEventListener("input", () => {
+   const fontSize = fontSizeCtrl.value + "px";
+   text.style.fontSize = fontSize;
+});
